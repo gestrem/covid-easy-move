222f7ee1936bf08f4ffdede6245699c2a75f5937
Guillaume ESTREM <guigui.estrem@hotmail.fr>
2020-04-11 15:39:27 +0200

    Initial Commit

---
+++ covid-easy-move/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  covid-easy-move
+//
+//  Created by Guillaume ESTREM on 4/11/20.
+//  Copyright © 2020 Guillaume ESTREM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
